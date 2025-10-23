ab98cc45eea6eb83212bb338abcfc01bdcfa2731
Yancey <1709185482@qq.com>
2025-10-24 07:06:45 +0800

    port to bbs 1.5.6

--- src/client/java/yancey/bbsaekeyframe/mixin/client/UIFilmPreviewMixin.java
+++ src/client/java/yancey/bbsaekeyframe/mixin/client/UIFilmPreviewMixin.java
@@ -7,16 +7,16 @@ package yancey.bbsaekeyframe.mixin.client;
 
 import mchorse.bbs_mod.l10n.keys.IKey;
 import mchorse.bbs_mod.l10n.keys.StringKey;
-import mchorse.bbs_mod.ui.film.UIFilmPanel;
 import mchorse.bbs_mod.ui.film.UIFilmPreview;
+import mchorse.bbs_mod.ui.framework.elements.IUIElement;
+import mchorse.bbs_mod.ui.framework.elements.UIElement;
 import mchorse.bbs_mod.ui.framework.elements.buttons.UIIcon;
 import mchorse.bbs_mod.ui.utils.icons.Icons;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import yancey.bbsaekeyframe.util.AEKeyframeGenerator;
 import yancey.bbsaekeyframe.util.ClipboardUtil;
 
@@ -29,19 +29,14 @@ public class UIFilmPreviewMixin {
     @Shadow
     public UIIcon recordVideo;
 
-    @Inject(method = "<init>",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lmchorse/bbs_mod/ui/framework/elements/buttons/UIIcon;context(Ljava/util/function/Consumer;)Lmchorse/bbs_mod/ui/framework/elements/UIElement;",
-                    ordinal = 2,
-                    shift = At.Shift.AFTER
-            ))
-    private void addCopyAEKeyframeMenu(UIFilmPanel filmPanel, CallbackInfo ci) {
+    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lmchorse/bbs_mod/ui/framework/elements/UIElement;add([Lmchorse/bbs_mod/ui/framework/elements/IUIElement;)V"))
+    private void addCopyAEKeyframeMenu(UIElement instance, IUIElement[] iuiElements) {
         recordVideo.context(menu -> menu.action(Icons.COPY, FILM_COPY_AE_KEYFRAME, () -> {
             if (AEKeyframeGenerator.lastKeyframeStr != null) {
                 ClipboardUtil.setClipboard(AEKeyframeGenerator.lastKeyframeStr);
             }
         }));
+        instance.add(iuiElements);
     }
 
 }
